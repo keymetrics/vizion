00e4d95c8232c1e214b9e9815bb6884eb4173ea5
Daniel Hermann <dth.dyver@googlemail.com>
2017-10-11 22:45:21 +0200

    fix cliCommand test

--- test/functional/cliCommand.test.js
+++ test/functional/cliCommand.test.js
@@ -3,18 +3,20 @@ var cliCommand = require("../../lib/cliCommand.js");
 
 describe('Functional: cliCommand', function () {
 
-    var folderWindows = "C:\\Program Files\\nodejs\\foobar";
-    var folderUnix = "/etc/node/foobar";
-
     it("ok", function () {
-        var target;
+        var target, folder;
 
-        if (/^win/.exec(process.platform))
-            target = "cd \"" + folderWindows + "\" && git status -s";
-        else
-            target = "cd '" + folderUnix + "';LC_ALL=en_US.UTF-8 git status -s";
+        if (/^win/.exec(process.platform)) {
+            folder = "C:\\Program Files\\nodejs\\foobar";
+            target = "cd \"" + folder + "\" && git status -s";
+        }
+        else {
+            folder = "/etc/node/foobar";
+            target = "cd '" + folder + "';LC_ALL=en_US.UTF-8 git status -s";
+        }
 
-        var result = cliCommand(folderWindows, "git status -s");
+
+        var result = cliCommand(folder, "git status -s");
         expect(target).to.eq(result);
     });
 
